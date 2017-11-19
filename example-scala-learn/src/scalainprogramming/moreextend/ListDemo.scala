1441368925ce69804e58a15d72c61d7df049ebb7
undergrowth <undergrowth@126.com>
2017-11-19 23:26:54 +0800

    scala 21 undone

---
+++ example-scala-learn/src/scalainprogramming/moreextend/ListDemo.scala
@@ -0,0 +1,13 @@
+package scalainprogramming.moreextend
+
+/**
+  * @description
+  * @author zhangwu
+  * @date 2017-11-19-18:56
+  * @version 1.0.0
+  */
+object ListDemo {
+  def main(args: Array[String]) {
+
+  }
+}
