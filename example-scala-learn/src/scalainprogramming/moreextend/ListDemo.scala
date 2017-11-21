94ea86fb85e44acaf84296cdb2d0695ba9c9d43b
undergrowth <undergrowth@126.com>
2017-11-21 21:56:35 +0800

    scala 16 done

--- example-scala-learn/src/scalainprogramming/moreextend/ListDemo.scala
+++ example-scala-learn/src/scalainprogramming/moreextend/ListDemo.scala
@@ -8,10 +8,24 @@ package scalainprogramming.moreextend
   */
 object ListDemo {
 
+  // head tail 运行时间都是常量
+  // init last 时间与列表长度成正比
+  // reverse
+  // /:----向左操作符  :\----向右操作符
   def display(l: List[Int]): Unit = {
+    println(l take 1)
+    println(l drop 1)
+    println(l.splitAt(1))
     println(l.head)
     println(l.tail)
     println(l.isEmpty)
+    l.map(_ + 1)
+  }
+
+  // 分而治之
+  def append(x: List[Int], y: List[Int]): List[Int] = x match {
+    case List() => y
+    case x :: xs1 => x :: append(xs1, y)
   }
 
   // list的所有元素都具有相同类型
@@ -23,5 +37,10 @@ object ListDemo {
     val li = 1 :: 2 :: Nil
     display(l)
     display(li)
+    // ::: 连接----右结合
+    display(l ::: li)
+    // 利用分而治之
+    display(append(l, li))
+    display(List.range(1,10,2))
   }
 }
