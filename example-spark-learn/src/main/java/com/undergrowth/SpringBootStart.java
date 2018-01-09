fcafb587ea8824f8f5876f9588707d6d08163a4e
zhangwu <undergrowth@126.com>
2018-01-09 18:16:00 +0800

    调整spark-learn 支持编译java/scala

---
+++ example-spark-learn/src/main/java/com/undergrowth/SpringBootStart.java
@@ -0,0 +1,16 @@
+package com.undergrowth;
+
+/**
+ * 启动
+ *
+ * @author zhangwu
+ * @version 1.0.0
+ * @date 2018-01-09-16:55
+ */
+public class SpringBootStart {
+
+    /*public static void main(String[] args) throws Exception {
+        SpringApplication.run(SpringBootStart.class, args);
+    }*/
+
+}
