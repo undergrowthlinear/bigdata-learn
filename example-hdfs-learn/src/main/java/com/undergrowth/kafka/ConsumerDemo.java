ed2193b52273bb5b41ffc625794453162fea2383
zhangwu <undergrowth@126.com>
2018-04-19 19:01:53 +0800

    add kafka

---
+++ example-hdfs-learn/src/main/java/com/undergrowth/kafka/ConsumerDemo.java
@@ -0,0 +1,39 @@
+package com.undergrowth.kafka;
+
+import java.util.Arrays;
+import java.util.Properties;
+import org.apache.kafka.clients.consumer.ConsumerRecord;
+import org.apache.kafka.clients.consumer.ConsumerRecords;
+import org.apache.kafka.clients.consumer.KafkaConsumer;
+
+/**
+ * test
+ *
+ * @author zhangwu
+ * @version 1.0.0
+ * @date 2018-04-19-18:25
+ */
+public class ConsumerDemo {
+    public static void main(String[] args) {
+        Properties properties = new Properties();
+        properties.put("bootstrap.servers", "s3.ds.tc:9092");
+        properties.put("group.id", "group-1");
+        properties.put("enable.auto.commit", "true");
+        properties.put("auto.commit.interval.ms", "1000");
+        properties.put("auto.offset.reset", "earliest");
+        properties.put("session.timeout.ms", "30000");
+        properties.put("key.deserializer", "org.apache.kafka.common.serialization.StringDeserializer");
+        properties.put("value.deserializer", "org.apache.kafka.common.serialization.StringDeserializer");
+
+        KafkaConsumer<String, String> kafkaConsumer = new KafkaConsumer<>(properties);
+        kafkaConsumer.subscribe(Arrays.asList("HelloWorld"));
+        while (true) {
+            ConsumerRecords<String, String> records = kafkaConsumer.poll(100);
+            for (ConsumerRecord<String, String> record : records) {
+                System.out.printf("offset = %d, value = %s", record.offset(), record.value());
+                System.out.println();
+            }
+        }
+
+    }
+}
